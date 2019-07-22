80f352c29f9dcaeddcb3ebbf92ef33941d07a9a6
Shaps Benkau <shapsuk@me.com>
2019-07-22 20:17:12 +0100

    Added a standard Xcode project to allow Carthage builds

---
+++ FlowLayout/FlowLayout.h
@@ -0,0 +1,18 @@
+//
+//  FlowLayout.h
+//  FlowLayout
+//
+//  Created by Shaps Benkau on 22/07/2019.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlowLayout.
+FOUNDATION_EXPORT double FlowLayoutVersionNumber;
+
+//! Project version string for FlowLayout.
+FOUNDATION_EXPORT const unsigned char FlowLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlowLayout/PublicHeader.h>
+
+
